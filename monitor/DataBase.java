2301434ad501ab58dc6d0590e4348c8b2a3c45a4
Marek Nedza <Marek.Nedza@fis.agh.edu.pl>
2014-06-17 11:07:13 +0200

    files structure fixed.

---
+++ monitor/DataBase.java
@@ -0,0 +1,497 @@
+package monitor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import sun.management.Sensor;
+
+/**
+ * Class to handle SQLite data base.
+ * There are 4 tables in db: Host, Sensor, Metric and Measurement.  
+ * @author Mietek
+ */
+public class DataBase {
+	
+	public static final String DRIVER = "org.sqlite.JDBC";
+    public static final String DB_URL = "jdbc:sqlite:monitor.db";
+    
+    private Connection conn;
+    
+    /// constructor
+    /**
+     * Create/connect to database monitor.db and invoke method to create tables.
+     */
+    public DataBase() {
+        try {
+            Class.forName(DataBase.DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Lack of JDBC driver");
+            e.printStackTrace();
+        }
+ 
+        try {
+            conn = DriverManager.getConnection(DB_URL);
+        } catch (SQLException e) {
+            System.err.println("Problem with open connection");
+            e.printStackTrace();
+        }
+        createTables();
+    }
+    
+    /// private methods
+    /**
+     * Create tables if not exists.
+     * @return true if no error occurs.
+     */
+    private boolean createTables()  {
+    	String createUser = "CREATE TABLE IF NOT EXISTS users ("
+    			+ "login text NOT NULL UNIQUE, password text, PRIMARY KEY (login) )";
+//        String createHost = "CREATE TABLE IF NOT EXISTS host ("
+//        		+ "id integer NOT NULL PRIMARY KEY AUTOINCREMENT, hostname text UNIQUE,	ip text)";
+//        String createSensor = "CREATE TABLE IF NOT EXISTS sensor ("
+//        		+ "id integer NOT NULL PRIMARY KEY AUTOINCREMENT, hostid integer NOT NULL, owner text NOT NULL UNIQUE,"
+//        		+ "sensorname text,	sensortype text, rpm integer, FOREIGN KEY (hostid) REFERENCES HOST (id),"
+//        		+ "FOREIGN KEY (owner) REFERENCES USER (login) )";
+        String createSensor = "CREATE TABLE IF NOT EXISTS sensor (id integer NOT NULL, login text NOT NULL,"
+        		+ "hostname text, hostip text, sensorname text, sensortype text, rpm integer,"
+        		+ "PRIMARY KEY (id), FOREIGN KEY (login) REFERENCES USERs (id) )";
+//        String createMetric = "CREATE TABLE IF NOT EXISTS metric ("
+//        		+ "id integer NOT NULL PRIMARY KEY AUTOINCREMENT, name text)";     
+//        String createMeasurement = "CREATE TABLE IF NOT EXISTS measurement ("
+//        		+ "id integer NOT NULL PRIMARY KEY AUTOINCREMENT, metricid integer NOT NULL, sensorid integer NOT NULL,"
+//        		+ "time text, value text, FOREIGN KEY (metricid) REFERENCES METRIC (id),"
+//        		+ "FOREIGN KEY (sensorid)	REFERENCES SENSOR (id) )";
+        String createMetric = "CREATE TABLE IF NOT EXISTS metric (id integer NOT NULL, sensorid integer NOT NULL,"
+        		+ "metricname text, time text, value text, PRIMARY KEY (id), FOREIGN KEY (sensorid)	REFERENCES SENSOR (id) )";
+        try {
+            Statement stat = conn.createStatement();
+            stat.executeUpdate(createUser);
+            //stat.executeUpdate(createHost);
+            stat.executeUpdate(createSensor);
+            stat.executeUpdate(createMetric);
+            stat.close();
+            //stat.executeUpdate(createMeasurement);
+        } catch (SQLException e) {
+            System.err.println("Error in creating tables");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
+    
+    /// public api
+    /**
+     * Add new user
+     * @param user to add
+     */
+    public void addUser(User user){
+    	try {
+        	Statement stat = conn.createStatement();
+        	stat.executeUpdate("INSERT INTO Users (login,password) VALUES ('"+user.login+"','"+user.password+"')");
+        	//conn.commit();
+        	stat.close();         
+        } catch (SQLException e) {
+        	System.err.println("add User err: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+   
+    /**
+     * Get list of available users.
+     * @return users list.
+     */
+    public List<User> getUsers(){
+    	List<User> users = new LinkedList<User>();
+        try {
+        	Statement stat = conn.createStatement();
+            ResultSet result = stat.executeQuery("SELECT * FROM users");
+            int id;
+            String login, password;
+            while(result.next()) {
+            	//id = result.getInt("id");
+                login = result.getString("login");
+                password = result.getString("password");            
+                users.add(new User(login, password));
+            }
+            result.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }     
+        return users;
+    }
+    
+    /**
+     * Get specific user by id
+     * @param userID user id
+     * @return User
+     */
+    public User getUser(String login){
+    	User user = null;
+    	try {
+        	Statement stat = conn.createStatement();
+            ResultSet result = stat.executeQuery("SELECT * FROM users where login='"+login+"'");
+            String password;
+            if(result.next()) {
+                user = new User(login = result.getString("login"), password = result.getString("password"));            
+            }
+            result.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }     
+        return user;
+    }
+    
+    /**
+     * Get list of available hosts.
+     * @return hosts list.
+     */
+    public List<Host> getHosts(){
+    	List<Host> hosts = new LinkedList<Host>();
+        try {
+        	Statement stat = conn.createStatement();
+            ResultSet result = stat.executeQuery("SELECT DISTINCT hostname, hostip FROM sensor");
+            String hostName, ip;
+            while(result.next()) {
+                hostName = result.getString("hostname");
+                ip = result.getString("hostip");            
+                hosts.add(new Host(hostName, ip));
+            }
+            result.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }     
+        return hosts;
+    }
+    
+    /**
+     * Get specific host by id.
+     * @param hostId host id
+     * @return requested host or null if there are no host with such id in db.
+     */
+//    public Host getHost(int hostID){
+//    	Host host = null;
+//    	try {
+//        	Statement stat = conn.createStatement();
+//        	ResultSet result = stat.executeQuery("SELECT * FROM host where id="+hostID);
+//        	if(result.next()){
+//        		host = new Host(result.getInt("id"), result.getString("hostname"), result.getString("ip"));
+//        	}
+//            stat.close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    	return host;
+//    }
+    
+    /**
+     * Get specific host by name
+     * @param hostName
+     * @return host
+     */
+    public Host getHost(String hostName){
+    	Host host = null;
+    	try {
+        	Statement stat = conn.createStatement();
+        	ResultSet result = stat.executeQuery("SELECT DISTINCT * FROM sensor where hostname='"+hostName+"'");
+        	if(result.next()){
+        		host = new Host(result.getString("hostname"), result.getString("hostip"));
+        	}
+        	result.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    	return host;
+    }
+    
+    /**
+     * Get sensors list of specific host
+     * @param hostName host name
+     * @return sensor list
+     */
+    public List<Sensor> getSensors(String hostName){
+    	List<Sensor> sensors = new LinkedList<Sensor>();
+        try {
+        	Statement stat = conn.createStatement();
+            ResultSet result = stat.executeQuery("SELECT DISTINCT * FROM sensor where hostname='"+hostName+"'");
+            int id;
+            String login, hostIP, sensorName, sensorType;
+            int rpm; 
+            while(result.next()) {
+                id = result.getInt("id");
+                login = result.getString("login");
+                hostIP = result.getString("hostip");
+                sensorName = result.getString("sensorname");
+                sensorType = result.getString("sensortype");
+                rpm = result.getInt("rpm");
+                sensors.add(new Sensor(id, login, hostName, hostIP, sensorName, sensorType, rpm));
+            }
+            result.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }      
+        return sensors;
+    }
+    
+    /**
+     * Get specific sensor of specific host, by hostID and sensorName
+     * @param hostName
+     * @param sensorName
+     * @return sensor
+     */
+    public Sensor getSensor(String hostName, String sensorName){
+    	Sensor sensor = null;
+    	try {
+        	Statement stat = conn.createStatement();
+        	ResultSet result = stat.executeQuery("SELECT * FROM sensor WHERE sensorname='"+sensorName+"'"
+        			+" AND hostname='"+hostName+"'");
+        	if(result.next()){
+         		sensor = new Sensor(result.getInt("id"), result.getString("login"), result.getString("hostName"),
+         				result.getString("hostip"), result.getString("sensorName"), result.getString("sensorType"),
+         				result.getInt("rpm"));
+        	}
+        	result.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    	return sensor;
+    }
+    
+    /**
+     * Get metrics list of specific sensor
+     * @param sensorID host id
+     * @return metrics list
+     */
+    public List<Metric> getMetrics(int sensorID){
+    	List<Metric> metrics = new LinkedList<Metric>();
+        try {
+        	Statement stat = conn.createStatement();
+            //ResultSet result = stat.executeQuery("SELECT * FROM metric m where m.id in (select metricid from measurement "
+            //		+ "where sensorID="+sensorID+")");
+        	ResultSet result = stat.executeQuery("SELECT DISTINCT metricname FROM metric where sensorid="+sensorID);
+            int id;
+            String metricName, time, value;
+            while(result.next()) {
+                //id = result.getInt("id");
+                metricName = result.getString("metricname");
+                //time = result.getString("time");
+                //value = result.getString("value");
+                metrics.add(new Metric(0, sensorID, metricName, "test", "test"));
+            }
+            result.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }      
+        return metrics;
+    }
+    
+    /**
+     * Get sepcific metric by sensorID and metricName
+     * @param sensorID
+     * @param metricName
+     * @return
+     */
+    public Metric getMetric(int sensorID, String metricName){
+    	Metric metric = null;
+    	try {
+        	Statement stat = conn.createStatement();
+//        	ResultSet result = stat.executeQuery("SELECT * FROM metric m where m.name='"+metricName+"' and m.id in (select metricid from measurement "
+//            		+ "where sensorID="+sensorID+")");
+        	ResultSet result = stat.executeQuery("SELECT * FROM metric where metricname='"+metricName+"' and sensorid="+sensorID);
+        	if(result.next()){
+         		metric = new Metric(result.getInt("id"), sensorID, metricName, result.getString("time"), result.getString("value"));
+        	}
+        	result.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    	return metric;
+    }
+    
+    public void addMetric(Metric metric){
+    	try {
+	    	Statement stat = conn.createStatement();
+	    	String query="INSERT INTO metric (sensorid,metricname,time,value)"+
+	    	"VALUES ("+metric.sensorID+", '"+metric.metricName+"', '"+metric.time+"', '"+metric.value+"')";
+	    	stat.executeUpdate(query);
+	    	//conn.commit();
+	        stat.close();        
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Delete metric pointed by parameter coumpoundMetricName
+     * @param coumpoundMetricName
+     * @throws SQLException
+     */
+    public void deleteCompoundMetric(String coumpoundMetricName){
+    	try {
+	    	Statement stat = conn.createStatement();
+	    	String query="DELETE FROM metric WHERE metricname='"+coumpoundMetricName+"'";
+	    	stat.executeUpdate(query);
+	    	//conn.commit();
+	        stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Get data of specific metric
+     * @param sensorID
+     * @param metricName
+     * @param amount
+     * @return metrics list
+     */
+    public List<Metric> getMetricData(int sensorID, String metricName, int amount){
+    	List<Metric> metrics = new LinkedList<Metric>();
+    	try {
+        	Statement stat = conn.createStatement();
+        	ResultSet result = stat.executeQuery("SELECT * FROM metric where metricname='"+metricName+"' and sensorid="+sensorID
+        			+ " order by time desc limit "+amount);
+        	while(result.next()){
+         		metrics.add (new Metric(result.getInt("id"), sensorID, metricName, result.getString("time"), result.getString("value")));
+        	}
+        	result.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    	return metrics;
+    }
+    
+    Random generator = new Random();
+    public void testDataInsertToMetric(){
+    	try {
+        	Statement stat = conn.createStatement();
+        	stat.executeQuery("INSERT INTO metric (sensorid,metricname,time,value) VALUES "
+        			+ "(1,'freeMemory','"+System.currentTimeMillis()+"'," + generator.nextInt(20)+")");
+        	//conn.commit();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+    
+    /// inner classes
+    /**
+     * User class represent User table in data base
+     */
+    class User {
+    	public String login;
+    	public String password;
+		public User(String login, String password) {
+			this.login = login;
+			this.password = password;
+		}
+    }
+    
+    /**
+     * Host class represent Host table in data base.
+     */
+    class Host {
+    	public String ip;
+    	public String hostName;
+    	Host(String hostName, String ip){
+    		this.ip = ip;
+    		this.hostName = hostName;
+    	}
+    }
+    
+    /**
+     * Metric class represent Metric table in data base.
+     */
+    class Sensor {
+    	public int id;
+    	public String login;
+    	public String hostName;
+    	public String hostIP;
+    	public String sensorName;
+    	public String sensorType;
+    	public int rpm;
+		public Sensor(int id, String login, String hostName, String hostIP, String sensorName, String sensorType, int rpm) {
+			this.id = id;
+			this.login = login;
+			this.hostName = hostName;
+			this.hostIP = hostIP;
+			this.sensorName = sensorName;
+			this.sensorType = sensorType;
+			this.rpm = rpm;
+		}
+    }
+    
+  /**
+  * Metric class represent Metric table in data base.
+  */
+ class Metric {
+ 	public int id;
+ 	public int sensorID;
+ 	public String metricName;
+ 	public String time;
+ 	public String value;
+	public Metric(int id, int sensorID, String metricName, String time, String value) {
+		this.id = id;
+		this.sensorID = sensorID;
+		this.metricName = metricName;
+		this.time = time;
+		this.value = value;
+	}	
+ }
+        
+//    /**
+//     * Metric class represent Metric table in data base.
+//     */
+//    class Metric {
+//    	public int id;
+//    	public String metricName;
+//    	Metric(int id, String metricName){
+//    		this.id = id;
+//    		this.metricName = metricName;
+//    	}
+//    }
+//    
+//    /**
+//     * Measurement class represent Measurement table in data base.
+//     */
+//    class Measurement {
+//    	public int id;
+//    	public int metricID;
+//    	public String time;
+//    	public double value;
+//    	Measurement(int id, int metricID, String time, double value){
+//    		this.id = id;
+//    		this.metricID = metricID;
+//    		this.time = time;
+//    		this.value = value;
+//    	}
+//    }
+}
---
+++ monitor/Monitor.java
@@ -0,0 +1,178 @@
+package monitor;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import monitor.DataBase.User;
+
+public class Monitor {
+
+	public static void main(String[] args) {
+		try {
+			new Monitor();
+		}
+		catch (Exception e){
+			System.err.println("Monitor Exception: "+e.getMessage());
+		}
+	}
+	
+	/// private fields
+	private RestServer restServer;
+	private DataBase dataBase;
+	
+	/**
+     * READABLE parameters in runtime - if necessary expand for more
+     */
+    private String monitorName, catalogURL;
+	
+	/// contructor
+	public Monitor() throws IOException{		
+		dataBase = new DataBase();	
+		readParamteres("configure.properties");
+		restServer = new RestServer(this.monitorName, dataBase);	
+		registerInCatalog();
+		//changeMonitorEntryInCatalog();
+		//deleteMonitorEntryInCatalog();
+	}
+	
+	/// private mtehods
+	/**
+	 * Register this monitor in Catalog.
+	 */
+	private void registerInCatalog(){	
+		try {
+			// Construct data
+			String data = "{\"name\":\""+this.monitorName+"\",\"ip\":\""+restServer.getMonitorIP()+":8889\"}";
+			String urlString = catalogURL+"/monitors";
+			URL url = new URL(urlString);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+		    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+		    wr.write(data);
+		    wr.flush();
+		    wr.close();
+		    int responseCode = conn.getResponseCode();
+			System.out.println("\nSending 'POST' request to URL : " + urlString);
+			System.out.println("Response Code : " + responseCode);   
+		} catch (Exception e) {
+			System.err.println("Monitor Exception in registerInCatalog");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Change Monitor ip in catalog
+	 */
+	private void changeMonitorEntryInCatalog(){
+		try {
+			// Construct data
+			String data = "{\"ip\":\"89.68.69.22:8898\"}";
+			String urlString = catalogURL+"/monitors/fatcat_2";
+			URL url = new URL(urlString);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+		    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+		    wr.write(data);
+		    wr.flush();
+		    wr.close();
+		    int responseCode = conn.getResponseCode();
+			System.out.println("\nSending 'POST' request to URL : " + urlString);
+			System.out.println("Response Code : " + responseCode);   
+		} catch (Exception e) {
+			System.err.println("Monitor Exception in registerInCatalog");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Delete Monitor Entry in Catalog
+	 */
+	private void deleteMonitorEntryInCatalog(){
+		try {
+			// Construct data
+			String urlString = catalogURL+"/monitors/"+monitorName;
+			URL url = new URL(urlString);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("DELETE");
+			conn.setDoOutput(true);
+		    int responseCode = conn.getResponseCode();
+			System.out.println("\nSending 'DELETE' request to URL : " + urlString);
+			System.out.println("Response Code : " + responseCode);   
+		} catch (Exception e) {
+			System.err.println("Monitor Exception in registerInCatalog");
+			e.printStackTrace();
+		}
+	}
+	
+	
+    /**
+     * READ parameters in runtime
+     * @param fileName
+     */
+    private void readParamteres(String fileName)
+    {
+    	String line = "";
+    	
+    	FileReader file = null;
+    	try 
+    	{
+    		file = new FileReader(fileName);
+		} 
+    	catch (FileNotFoundException e)
+    	{
+    		System.err.format("Exception occurred trying to open '%s'.", fileName);
+			e.printStackTrace();
+			System.exit(1);
+		}
+    	
+    	BufferedReader buffer = new BufferedReader(file);
+    	try 
+    	{
+    		monitorName = buffer.readLine();
+    		catalogURL = buffer.readLine();
+    		List<User> users = dataBase.getUsers();
+    		boolean exists = false;
+    		while((line = buffer.readLine()) != null)
+    		{
+    			String [] userData = line.split(":");
+    			exists = false;
+    			for(User user : users){
+    				if (user.login.equals(userData[0])){
+    					exists = true;
+    					break;
+    				}
+    			}
+    			
+    			if (!exists)
+    				dataBase.addUser(dataBase.new User (userData[0],userData[1])); 
+    	    }
+    	} 
+    	catch (IOException e)
+    	{
+    		System.err.format("Exception occurred trying to read '%s'.", fileName);
+    		e.printStackTrace();
+	        System.exit(2);
+    	}
+	    
+	    try 
+	    {
+	    	file.close();
+	    } 
+	    catch (IOException e) 
+	    {
+	    	System.err.format("Exception occurred trying to close '%s'.", fileName);
+	    	e.printStackTrace();
+	        System.exit(3);
+	    }
+	    
+    }
+}
